5ad2085d4f10a3a5bdf83ac502f946c8db48111f
Angel Cochachez <acochachez@gmail.com>
2020-09-24 17:42:26 -0700

    Added an array for component to show inside slideshow

--- slideshow/src/Slider.js
+++ slideshow/src/Slider.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './slider.scss';
 
 
 function Slider(){
     //let's create an array for component to show inside the slider
-    let sliderArr=[1,2,3,4,5]//just numbers for now
+    let sliderArr=[1,2,3,4,5];//just numbers for now
     //add buttons
-    const goLeft=()=>{};
-    const goRight=()=>{};
+    const[x, setX]=useState(0);
+    const goLeft=()=>{
+        console.log(x)
+        x===0 ? setX(-100(sliderArr.length-1)):setX(x+100);
+    };
+    const goRight=()=>{
+        console.log(x)
+        //sliderArr.lenght was used so the input can be dynamic
+        x===-100*(sliderArr.length-1)?setX(0):setX(x-100);
+    };
     return(
         <div className="slider">
         {
-            sliderArr.map((item,index)=>{
+            sliderArr.map((item, index)=>{
                 return(
                     <div key={index} className="slide">
                         {item}
